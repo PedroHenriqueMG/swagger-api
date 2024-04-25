dfe71a7d3e6202bfaceabfc54f51b949d29e78d4
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-04-25 17:07:39 -0300

    add: swagger-ui-express

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import Express from "express";
+import swaggerui from "swagger-ui-express";
+import { document } from "./swagger/swagger";
 
 const app = Express();
 app.use(Express.json());
+const PORT = 8080;
 
 app.get("/", (req, res) => {
   res.send("API rodando");
+});
+
+app.use("/docs", swaggerui.serve, swaggerui.setup(document));
+
+app.listen(PORT, () => {
+  console.log(`API rodando na porta: ${PORT}`);
 });
 
 export default app;
